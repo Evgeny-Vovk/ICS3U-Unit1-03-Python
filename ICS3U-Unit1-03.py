152ae9626528a60cdf1f2df00269b3912006f3ff
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-15 15:05:53 +0000

    finished Unit-1-03 python

---
+++ ICS3U-Unit1-03.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# ICS3U-Unit1-03.py File, learning how style properly
+
+
+def main():
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
